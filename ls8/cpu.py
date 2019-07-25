c266938d85157044b0fd51dae32b100dd3e6f369
Michael <ney.quentin.michael@gmail.com>
2019-07-24 17:32:21 -0700

    Finished adding PUSH and POP handlers. Separted each type of action into an function, and alu handles math functions.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -11,6 +11,13 @@ class CPU:
         self.ram = [00000000] * 256
         self.reg = [0] * 8
         self.pc = 0
+        self.sp = 0xF4
+        self.branchtable = {}
+        self.branchtable[0b00000001] = self.handleHLT
+        self.branchtable[0b10000010] = self.handleLDI
+        self.branchtable[0b01000111] = self.handlePRN
+        self.branchtable[0b01000101] = self.handleStackPush
+        self.branchtable[0b01000110] = self.handleStackPop
 
     def load(self):
         """Load a program into memory."""
@@ -31,10 +38,21 @@ class CPU:
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
-        if op == "ADD":
+        ADD = 0b10100000
+        SUB = 0b10100001
+        MUL = 0b10100010
+        DIV = 0b10100011
+        if op == ADD:
             self.reg[reg_a] += self.reg[reg_b]
-        # elif op == "SUB": etc
+        elif op == SUB:
+            self.reg[reg_a] -= self.reg[reg_b]
+        elif op == MUL:
+            self.reg[reg_a] *= self.reg[reg_b]
+        elif op == DIV:
+            if not self.reg[reg_b]:
+                print("Error: You are not allowed to divide a number by 0.")
+                sys.exit()
+            self.reg[reg_a] /= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -64,15 +82,29 @@ class CPU:
     def ram_write(self, MAR, MDR):
         self.ram[MAR] = MDR
 
+    def handleHLT(self):
+        print('Process Ending.')
+        sys.exit()
+
+    def handleLDI(self, operand_1, operand_2):
+        self.reg[operand_1] = operand_2
+
+    def handlePRN(self, operand_1):
+        print(self.reg[operand_1])
+
+    def handleStackPush(self, operand_1):
+        self.sp -= 1
+        reg_value = self.reg[operand_1]
+        self.ram[self.sp] = reg_value
+
+    def handleStackPop(self, operand_1):
+        ram_value = self.ram[self.sp]
+        self.reg[operand_1] = ram_value
+        self.sp += 1
+
     def run(self):
         """Run the CPU."""
         on = True
-
-        # Instructions
-        HLT = 0b00000001
-        LDI = 0b10000010
-        PRN = 0b01000111
-        MUL = 0b10100010
 
         while on:
             IR = self.ram[self.pc]
@@ -81,27 +113,17 @@ class CPU:
             operand_1 = self.ram_read(self.pc + 1)
             operand_2 = self.ram_read(self.pc + 2)
 
-            if IR > 0b01111111:
-                # Check LDI instruction
-                if IR == LDI:
-                    self.reg[operand_1] = operand_2
-
-                if IR == MUL:
-                    self.reg[operand_1] = self.reg[operand_1] * self.reg[operand_2]
-
-                # requires 2 operands if 64 or higher, so we add 3 to make the current instruction the one after all operands involved.
-                self.pc += 3
-
-            # single operand
-            elif IR < 0b01111111 and IR > 0b00111111:
-                if IR == PRN:
-                    print(self.reg[operand_1])
-
-                self.pc += 2
-
+            operand_count = (IR & 0b11000000) >> 6
+            alu_number = (IR & 0b00100000) >> 5
+            if alu_number:
+                self.alu(IR, operand_1, operand_2)
+            elif operand_count == 2:
+                self.branchtable[IR](operand_1, operand_2)
+            elif operand_count == 1:
+                self.branchtable[IR](operand_1)
+            elif operand_count == 0:
+                self.branchtable[IR]()
             else:
-                if IR == HLT:
-                    on = False
-                    break
-
-                self.pc += 1
+                self.handleHLT()
+            self.pc += 1
+            self.pc += operand_count
